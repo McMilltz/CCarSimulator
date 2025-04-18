e5d11d01f9d2d5874c7ec5edb9223646691d74ea
Nils Keuchel <nils@keuchel.de>
2025-04-18 14:35:02 +0200

    Version1

--- src/main.c
+++ src/main.c
@@ -1,41 +1,60 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_events.h>
+#include <SDL2/SDL_keyboard.h>
+#include <SDL2/SDL_keycode.h>
+#include <SDL2/SDL_rect.h>
 #include <SDL2/SDL_render.h>
+#include <SDL2/SDL_surface.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <SDL2/SDL_image.h>
+#include "Car/Car.h"
+#include "Constants.h"
+#include "Input.h"
 
 int main(){
   printf("Hello World!\n");
 
-  #define WHITE 255,255,255
-
   SDL_Window* window;
   SDL_Renderer* renderer;
 
-
+  printf("%d\n",SDL_Init(SDL_INIT_EVERYTHING));
+  printf("%d\n",SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer));
 
   SDL_Event e;
 
-  SDL_Init(SDL_INIT_EVERYTHING);
+  CAR_Car car;
+  car.renderer = &renderer;
+  const char* carPictureFilename = "./Media/Car_Topdown_Red.png";
+  const char* tirePictureFilename = "./Media/Tire.png";
+  const char* tireSideViewFilename = "./Media/TireSideView.png";
+  CAR_loadTexture(&car, carPictureFilename, tirePictureFilename, tireSideViewFilename);
+  CAR_Instantiate(&car, 300, 400, 0, B, L);
 
-  SDL_Rect rectangle = {20, 400, 50, 80};
-  SDL_CreateWindowAndRenderer(800, 600, 0, &window, &renderer);
   bool running = true;
+
 
   while(running){
     while(SDL_PollEvent(&e)){
       if(e.type == SDL_QUIT) {
         running = false;
+      }else{
+        INP_update(&car.carInput, &e);
       }
     }
+
+    // geometry.x += sin(rotation) * moveSpeed;
+    // geometry.y -= cos(rotation) * moveSpeed;
+    CAR_Update(&car, TIME_STEP);
+ 
     SDL_SetRenderDrawColor(renderer, 70, 70, 70, 200); 
     SDL_RenderClear(renderer);
 
+    CAR_draw(&car);
     SDL_SetRenderDrawColor(renderer,WHITE,255);
-    SDL_RenderDrawRect(renderer, &rectangle);
 
     SDL_RenderPresent(renderer);
-    SDL_Delay(10);
+    SDL_Delay(TIME_STEP*1000.0);
   }
 
 
